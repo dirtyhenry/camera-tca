1ebe55f0a286f004b3722b81e36b1a3cf470d3c2
Mick F <mick@mickf.net>
2023-07-18 19:27:06 +0200

    feat: 🎸 Simplification

--- CameraTCA/Root.swift
+++ CameraTCA/Root.swift
@@ -6,47 +6,31 @@ import UniformTypeIdentifiers
 
 struct RootFeature: ReducerProtocol {
     struct State {
-        @PresentationState var takePhoto: ImagePickerFeature.State?
-        var latestPhoto: UIImage?
+        var latestPhotoData: Data?
         var count: Int
     }
-
+    
     enum Action {
-        case takePhotoButtonTapped
-        case usePhoto(PresentationAction<ImagePickerFeature.Action>)
         case incrementButtonTapped
+        case loadDummyButtonTapped
     }
-
+    
     var body: some ReducerProtocolOf<Self> {
         Reduce<State, Action> { state, action in
             switch action {
             case .incrementButtonTapped:
                 state.count += 1
                 return .none
-
-            case .takePhotoButtonTapped:
-                print("takePhotoButtonTapped \(state.count)")
-                state.takePhoto = ImagePickerFeature.State()
-                state.count += 1
-                return .none
-
-            case let .usePhoto(.presented(.delegate(.usePhoto(newPhoto)))):
-                print("usePhoto \(state.count)")
-                state.latestPhoto = newPhoto
-                state.takePhoto = nil
-                state.count += 1
-                return .none
-
-            case .usePhoto(.presented(.delegate(.cancel))):
-                state.takePhoto = nil
-                return .none
-
-            case .usePhoto:
+                
+            case .loadDummyButtonTapped:
+                guard let image = UIImage(named: "DummyImage"),
+                      let imageData = image.jpegData(compressionQuality: 1) else {
+                    fatalError("No image found.")
+                }
+                state.latestPhotoData = imageData
+                print("Set data")
                 return .none
             }
-        }
-        .ifLet(\.$takePhoto, action: /Action.usePhoto) {
-            ImagePickerFeature()
         }
     }
 }
@@ -60,35 +44,20 @@ struct RootView: View {
     var body: some View {
         NavigationStack {
             WithViewStore(store, observe: { $0 }) { viewStore in
-                if viewStore.latestPhoto == nil {
-                    Button("Take Photo \(viewStore.count)") {
-                        viewStore.send(.takePhotoButtonTapped)
-                    }
-                    Button("Increment \(viewStore.count)") {
-                        viewStore.send(.incrementButtonTapped)
+                if viewStore.latestPhotoData == nil {
+                    VStack(spacing: 32) {
+                        Button("Increment \(viewStore.count)") {
+                            viewStore.send(.incrementButtonTapped)
+                        }
+                        Button("Load Dummy Photo") {
+                            viewStore.send(.loadDummyButtonTapped)
+                        }
+                        Text("\(viewStore.latestPhotoData == nil ? "Nil" : "Data")")
                     }
                 } else {
-                    VStack {
-                        Image(uiImage: viewStore.latestPhoto!)
-                        Button("Retake Photo") {
-                            viewStore.send(.takePhotoButtonTapped)
-                        }
-                    }
+                    Text("PHOTO")
                 }
             }
-        }
-        .sheet(
-            store: store.scope(
-                state: \.$takePhoto,
-                action: { .usePhoto($0) }
-            )) { takePhotoStore in
-                NavigationStack {
-                    ImagePickerView(
-                        store: takePhotoStore,
-                        sourceType: .camera,
-                        mediaTypes: [UTType.image.identifier]
-                    )
-                }
         }
     }
 }
